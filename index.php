f6fd22d80e43284a83bcc809eb17a33f2dc5eeba
Eric Masiello <eric.j.masiello@gmail.com>
2017-05-13 10:45:35 -0400

    Add shortcode svg support

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: WP Inline SVG Shortcode
+Plugin URI: https://github.com/ericmasiello/wp-inline-svg-shortcode
+Description: Allows you to insert inline SVGs using shortcodes
+Version: 0.0.1
+Author: Eric Masiello
+Author URI: http://www.synbydesign.com
+License: GPL
+Copyright: Eric Masiello
+Credit: 
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+function inline_svg_fn( $atts, $content = NULL ) {
+  extract( shortcode_atts( array( 'id' => '', 'class' => '' ), $atts ) );
+  $output = '<svg class="' . $class . '"><use xlink:href="#' . $id . '" /></svg>';
+  return $output;
+}
+
+add_shortcode( "inline_svg", "inline_svg_fn" );
+?>
